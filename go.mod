82dd9d0a8526c4878f7360b64db57fd13ee0e9c8
Mateusz Kolodziejczyk <mateuszlk1998@gmail.com>
2021-11-01 12:12:57 +0000

    Move ffmpeg transcoding into own function

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module github.com/mateusz-kolodziejczyk/streaming-application
 
 go 1.17
 
-require github.com/u2takey/ffmpeg-go v0.3.0
-
 require (
+	github.com/u2takey/ffmpeg-go v0.3.0
 	github.com/aws/aws-sdk-go v1.41.5 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,30 +1,36 @@
 package main
 
 import (
+	"fmt"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"log"
 )
-func main() {
-	err := ffmpeg.Input("rtmp://192.168.0.66/live/test").
-		Output("./test_videos/stream/stream_%v/stream.m3u8",
+
+const ServerAddress string = "192.168.0.66"
+const StreamDirectory string = "./test_video/stream"
+func hlsStream(streamKey string, streamName string){
+	err := ffmpeg.Input(fmt.Sprintf("rtmp://%s/live/%s", ServerAddress, streamKey)).
+		Output(fmt.Sprintf("%s/%s.m3u8", StreamDirectory, streamName),
 			ffmpeg.KwArgs{
-			"c:a": "aac",
-			"b:a": "128k",
-			"ac": "2",
-			"c:v": "libx264",
-			"preset": "veryfast",
-			"b:v":"1M",
-			"f": "hls",
-			"hls_time": "4",
-			"hls_init_time": "12",
-			"hls_list_size": "3",
-			"hls_playlist_type": "event",
-			"hls_segment_type": "mpegts",
-			"hls_segment_filename": "./test_videos/stream/stream_%v/data%02d.ts"}).
+				"c:a": "aac",
+				"b:a": "128k",
+				"ac": "2",
+				"c:v": "libx264",
+				"preset": "veryfast",
+				"b:v":"1M",
+				"f": "hls",
+				"hls_time": "4",
+				"hls_init_time": "12",
+				"hls_list_size": "3",
+				"hls_playlist_type": "event",
+				"hls_segment_type": "mpegts",
+				"hls_segment_filename": fmt.Sprintf("%s/%s", StreamDirectory, streamName) + "%02d.ts"}).
 		OverWriteOutput().Run()
 	if err != nil {
 		log.Fatal(err)
-		panic(err)
 	}
+}
+func main() {
+	hlsStream("MyStream", "firstStream")
 }
 
