3f2753cee338ab0c4f8b54c36fade0c7a3bbd412
mateusz-kolodziejczyk <marcleclerc2000@gmail.com>
2021-11-07 23:09:37 +0000

    Fix segment length

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module github.com/mateusz-kolodziejczyk/streaming-application
 
 go 1.17
 
+// Using a fork of the original ffmpeg module as it had a broken import
 require (
-	github.com/u2takey/ffmpeg-go v0.3.0
-	github.com/aws/aws-sdk-go v1.41.5 // indirect
+	github.com/mateusz-kolodziejczyk/ffmpeg-go v0.3.1
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/u2takey/go-utils v0.0.0-20210821132353-e90f7c6bacb5 // indirect
 )
+
+require github.com/u2takey/go-utils v0.0.0-20210821132353-e90f7c6bacb5 // indirect
--- main.go
+++ main.go
@@ -2,35 +2,43 @@ package main
 
 import (
 	"fmt"
-	ffmpeg "github.com/u2takey/ffmpeg-go"
+	ffmpeg "github.com/mateusz-kolodziejczyk/ffmpeg-go"
 	"log"
+	"os/exec"
 )
 
-const ServerAddress string = "192.168.0.66"
-const StreamDirectory string = "./test_video/stream"
+const ServerAddress string = "127.0.0.1"
+const StreamDirectory string = "stream"
+const ServerDirectory string = "/usr/local/nginx/html"
 func hlsStream(streamKey string, streamName string){
 	err := ffmpeg.Input(fmt.Sprintf("rtmp://%s/live/%s", ServerAddress, streamKey)).
-		Output(fmt.Sprintf("%s/%s.m3u8", StreamDirectory, streamName),
+		Output(fmt.Sprintf("%s/%s/%s.m3u8", ServerDirectory, StreamDirectory, streamName),
 			ffmpeg.KwArgs{
 				"c:a": "aac",
 				"b:a": "128k",
 				"ac": "2",
 				"c:v": "libx264",
 				"preset": "veryfast",
-				"b:v":"1M",
+				"b:v":"5M",
 				"f": "hls",
-				"hls_time": "4",
-				"hls_init_time": "12",
-				"hls_list_size": "3",
+				"hls_time": "5",
+				"hls_wrap": "5",
 				"hls_playlist_type": "event",
 				"hls_segment_type": "mpegts",
-				"hls_segment_filename": fmt.Sprintf("%s/%s", StreamDirectory, streamName) + "%02d.ts"}).
+				"g": "4",
+				"hls_list_size": "5",
+				"hls_segment_filename": fmt.Sprintf("%s/%s/%s",ServerDirectory, StreamDirectory, streamName) + "%03d.ts"}).
 		OverWriteOutput().Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 func main() {
-	hlsStream("MyStream", "firstStream")
+	cmd := exec.Command("/bin/sh", "-c", "rm " + fmt.Sprintf("%s/%s/*", ServerDirectory, StreamDirectory))
+	err := cmd.Run()
+	if err != nil{
+		log.Fatal(err)
+	}
+	hlsStream("cool", "firstStream")
 }
 
